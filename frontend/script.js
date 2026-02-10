28ad60a00c0cb6d84542261e84270ad31ae4b19d
Perera1325 <bamunuge2002@gmail.com>
2026-02-10 22:17:02 +0530

    Day 4: connect frontend to live Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -74,7 +74,8 @@ async function sendMessage() {
   chat.scrollTop = chat.scrollHeight;
 
   try {
-    const res = await fetch("http://127.0.0.1:8000/chat", {
+    const res = await fetch("https://emo-bot-backend.onrender.com/chat", {
+
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: msg })
